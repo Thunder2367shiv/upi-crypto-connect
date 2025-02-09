32c49660adee35ce82c2c8fe31c3036d4a318e11
thunder2367shiv <2022kuec2034@iiitkota.ac.in>
2025-02-10 03:41:09 +0530

    signup pages changes

--- src/app/Authenticate/page.jsx
+++ src/app/Authenticate/page.jsx
@@ -5,7 +5,8 @@ import OtpLogin from "@/components/login_signup";
 
 const Login = () => {
   return (
-    <div className="flex items-center justify-center w-full min-h-screen bg-gradient-to-b from-gray-900 via-gray-800 to-black p-6">
+    <div className="flex flex-col items-center justify-center w-full min-h-screen bg-gradient-to-b from-gray-900 via-gray-800 to-black p-6">
+      <div className="text-[#35b0ac] text-4xl font-bold">User Authentication Hub</div>
       {/* Increase the width of the parent container */}
       <div className="w-full max-w-6xl">
         <OtpLogin />
